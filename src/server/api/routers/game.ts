354eeb7d739c8f2c918d963f154bea78c56e96b3
Travis Saylor <travis.saylor@gmail.com>
2023-12-18 17:40:29 -0500

    Add game router

---
+++ src/server/api/routers/game.ts
@@ -0,0 +1,27 @@
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { createGameSchema, games } from "~/server/db/schema";
+
+export const gameRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.query.games.findMany();
+  }),
+  getById: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.query.games.findFirst({
+        where: (game) => eq(game.id, input.id),
+      });
+    }),
+  create: publicProcedure
+    .input(createGameSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(games).values(input);
+    }),
+});
